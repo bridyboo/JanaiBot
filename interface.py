2e10ffc1d78a3a86d8a815b3f5131f110fe2947e
matthdar <matthewdarmadi@gmail.com>
2024-01-18 01:18:06 -0800

    speech is now added to the janaiBot

--- interface.py
+++ interface.py
@@ -1,11 +1,16 @@
 from JanaiBot import chatbot
+import speech
 
 janai = chatbot.ChatBot()
 chatting = True
-while(chatting):
-	question = input("user: ")
+
+while (chatting):
+	question = speech.speech_to_text()
+	print("User:", question)
+
 	bot_response = janai.queryBot(question)
 	print("Janai:", bot_response)
 
-	if question == 'end program':
+	if 'end program' in question.lower():
 		break
+
